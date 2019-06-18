458d396c16877a7514b2c58e1107e63d952b41a8
HenriqueFQuick <henrique.fquick@gmail.com>
2019-06-18 18:27:18 -0300

    Update

--- Cryptography.py
+++ Cryptography.py
@@ -3,77 +3,86 @@ import io
 import time
 
 def encryptNewFile(name, secureKey):
-    with open(name,'rb') as reader:
-        reader.seek(4)
-        stri = reader.readline()
-        lenf = len(stri)
-        stra =[]
-        for i in range (lenf):
-            stra.append((stri[i]) + 4)    
-        strk = []
-        for i in range (lenf):
-            strk.append(stra[i].to_bytes(1, byteorder="big", signed="True"))
+    try:
+        with open(name,'rb') as reader:
+            reader.seek(4)
+            stri = reader.readline()
+            lenf = len(stri)
+            stra =[]
+            for i in range (lenf):
+                stra.append((stri[i]) + 4)    
+            strk = []
+            for i in range (lenf):
+                strk.append(stra[i].to_bytes(1, byteorder="big", signed="True"))
 
-    with open(name,'rb+') as writer:
-        writer.seek(4)
-        for i in range(lenf):
-            writer.write(strk[i])
+        with open(name,'rb+') as writer:
+            writer.seek(4)
+            for i in range(lenf):
+                writer.write(strk[i])
+    except OverflowError:
+        print("\nErro de OverFlow")
 
 def encrypt():
-    name = input("Qual o nome do arquivo? ")
-    with open(name,'rb') as reader:
+    try:
+        name = input("Qual o nome do arquivo? ")
+        with open(name,'rb') as reader:
 
-        secureKey = random.randrange(900000)
-        print("Sua chave de criptografia eh: ",secureKey)
-        secureKey = secureKey.to_bytes(4,byteorder='big', signed=True)
+            secureKey = random.randrange(900000)
+            print("Sua chave de criptografia eh: ",secureKey)
+            secureKey = secureKey.to_bytes(4,byteorder='big', signed=True)
 
-        stri = reader.readline()
-        lenf = len(stri)
-        stra =[]
-        for i in range (lenf):
-            stra.append((stri[i]) + 4)    
-        strk = []
-        for i in range (lenf):
-            strk.append(stra[i].to_bytes(1, byteorder="big", signed="True"))
+            stri = reader.readline()
+            lenf = len(stri)
+            stra =[]
+            for i in range (lenf):
+                stra.append((stri[i]) + 4)    
+            strk = []
+            for i in range (lenf):
+                strk.append(stra[i].to_bytes(1, byteorder="big", signed="True"))
 
-    with open(name,'rb+') as writer:
-        writer.write(secureKey)
-        writer.seek(4)
-        for i in range(lenf):
-            writer.write(strk[i])
+        with open(name,'rb+') as writer:
+            writer.write(secureKey)
+            writer.seek(4)
+            for i in range(lenf):
+                writer.write(strk[i])
+    except OverflowError:
+        print("\nErro de OverFlow")
 
         
 def descrypt():
-    name = input("Qual eh o nome do arquivo? ")
-    tentativas = 3
-    while tentativas > 0:
-        givenkey = int(input("Qual eh a sua chave de criptografia? "))
-        lenf = 0
-        with open(name, 'rb') as file:
-            securkey = file.read(4)
-            securekey = int.from_bytes(securkey, byteorder="big", signed="True")
-            if securekey == givenkey:
-                tentativas = 0
-                file.seek(4)
-                stri = file.readline()
-                lenf = len(stri)
-                stra =[]
-                for i in range (lenf):
-                    stra.append((stri[i]) - 4)    
-                file.seek(0)
-                strk = []
-                for i in range (lenf):
-                    strk.append(stra[i].to_bytes(1, byteorder="big", signed="True"))  
-            else:
-                tentativas = tentativas - 1
-                print("Key errada")  
-                time.sleep(1)
+    try:
+        name = input("Qual eh o nome do arquivo? ")
+        tentativas = 3
+        while tentativas > 0:
+            givenkey = int(input("Qual eh a sua chave de criptografia? "))
+            lenf = 0
+            with open(name, 'rb') as file:
+                securkey = file.read(4)
+                securekey = int.from_bytes(securkey, byteorder="big", signed="True")
+                if securekey == givenkey:
+                    tentativas = 0
+                    file.seek(4)
+                    stri = file.readline()
+                    lenf = len(stri)
+                    stra =[]
+                    for i in range (lenf):
+                        stra.append((stri[i]) - 4)    
+                    file.seek(0)
+                    strk = []
+                    for i in range (lenf):
+                        strk.append(stra[i].to_bytes(1, byteorder="big", signed="True"))  
+                else:
+                    tentativas = tentativas - 1
+                    print("Key errada")  
+                    time.sleep(0.8)
 
-        with open(name,'wb') as writer:
-            for i in range(lenf):
-                writer.write(strk[i])
-            print("\nArquivo descriptografado com sucesso!\n")
-
+            with open(name,'wb') as writer:
+                for i in range(lenf):
+                    writer.write(strk[i])
+                print("\nArquivo descriptografado com sucesso!\n")
+                time.sleep(0.8)
+    except OverflowError:
+        print("\nErro de OverFlow")
 
 def writeFile():
     try:
@@ -81,18 +90,21 @@ def writeFile():
         arq = open(name,'wb')
     except FileExistsError:
         print("Arquivo ja existe")
-    write = input("Digite o que deseja escrever no arquivo: ")
-    secureKey = random.randrange(900000)
-    print("Sua chave de criptografia eh: ",secureKey)
-    arq.seek(0)
-    secureKey = secureKey.to_bytes(4,byteorder='big', signed=True)
-    arq.write(secureKey)
-    write = str.encode(write)
-    arq.seek(4)
-    arq.write(write)
-    arq.close()
-    encryptNewFile(name,secureKey)
-    time.sleep(1)
+    try:
+        write = input("Digite o que deseja escrever no arquivo: ")
+        secureKey = random.randrange(900000)
+        print("Sua chave de criptografia eh: ",secureKey)
+        arq.seek(0)
+        secureKey = secureKey.to_bytes(4,byteorder='big', signed=True)
+        arq.write(secureKey)
+        write = str.encode(write)
+        arq.seek(4)
+        arq.write(write)
+        arq.close()
+        encryptNewFile(name,secureKey)
+        time.sleep(0.8)
+    except OverflowError:
+        print("\nErro de OverFlow")
 
 def menu():
     sair = False
@@ -122,7 +134,7 @@ def menu():
             sair = True
         else:
             print("\n\nOpcao invalida!!!\n")
-            time.sleep(1)
+            time.sleep(0.8)
             sair = False
         
 menu()
